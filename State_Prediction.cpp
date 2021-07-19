06927c5a22598c45fdf4f5e880fa23190b345d2a
Iaryan-21 <86413005+Iaryan-21@users.noreply.github.com>
2021-07-19 23:51:09 +0530

    Update State_Prediction.cpp

--- State_Prediction.cpp
+++ State_Prediction.cpp
@@ -14,3 +14,8 @@ int main()
   printf("[%f, %f]", new_mean, new_var );
   return 0;
 }
+// the var1 , var2 , mean1 and mean2 values are needed to be inserted in line13 insetad of the variables
+// mean1 = mean of the prior belief
+// var1 = variance of the prior belief
+// mean1 = mean of the measurement
+// mean2 = variance of the measurement
